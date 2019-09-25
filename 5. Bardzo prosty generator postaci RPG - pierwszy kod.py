e199b6d193f43a5ef25b541c347e601856fcd6da
KrystianKrajewski <53577526+KrystianKrajewski@users.noreply.github.com>
2019-09-26 01:37:57 +0200

    Update 5. Bardzo prosty generator postaci RPG - pierwszy kod.py

--- 5. Bardzo prosty generator postaci RPG - pierwszy kod.py
+++ 5. Bardzo prosty generator postaci RPG - pierwszy kod.py
@@ -1,162 +1,162 @@
 import random, os
 print("Witaj w Generatorze Postaci v1!\nWybierz imię dla swojej postaci nim przejdziemy dalej:")
-lista_ras = ["elf", "człowiek", "krasnolud"]
-lista_klas = ["wojownik", "strzelec", "mag"]
-cechy_postaci = {"Zręczność":5,"Siła":5, "Intelekt":5}
-dostepny_orez = {"wojownik":("miecz", "topór"), "strzelec":("łuk", "kusza"), "mag":("kostur")}
-nazwa_postaci = input()
-class ZlyWyborException(Exception):
-    def __init__(self, wprowadzana):
-        super().__init__("Wprowadzona treść: {}, nie jest jedną z opcji do wyboru".format(wprowadzana))
-class ZlyTypDamychException(Exception):
-    def __init__(self, wprowadzana):
-        super().__init__("Wprowadzone dane \"{}\" nie są danymi tekstowymi!".format(wprowadzana))
-def wybor_rasy():
+race_list = ["elf", "człowiek", "krasnolud"]
+clas_list = ["wojownik", "strzelec", "mag"]
+char_traits = {"Zręczność":5,"Siła":5, "Intelekt":5}
+weapons = {"wojownik":("miecz", "topór"), "strzelec":("łuk", "kusza"), "mag":("kostur")}
+ch_name = input()
+class WrongChoiceException(Exception):
+    def __init__(self, entry):
+        super().__init__("Wprowadzona treść: {}, nie jest jedną z opcji do wyboru".format(entry))
+class WrongDataTypeException(Exception):
+    def __init__(self, entry):
+        super().__init__("Wprowadzone dane \"{}\" nie są danymi tekstowymi!".format(entry))
+def race_choice():
     print("Pierwszym krokiem do stworzenia Twojej postaci będzie wybór jednej z 3 ras\n(krasnoluda, elfa lub człowieka). By tego"
           "dokonać wpisz nazwę rasy:")
-    wprowadzana = input().lower()
-    if type(wprowadzana) != str:
-        raise ZlyTypDamychException(wprowadzana)
-    elif wprowadzana in lista_ras:
-        return wprowadzana
+    entry = input().lower()
+    if type(entry) != str:
+        raise WrongDataTypeException(entry)
+    elif entry in race_list:
+        return entry
     else:
-        raise ZlyWyborException(wprowadzana)
-wybrana_rasa = wybor_rasy()
-print(wybrana_rasa)
-def wybor_klasy():
-    print("Czas byś wybrał klasę dla swojej postaci. Tak jak poprzednio do wyboru masz 3 opcje {}".format(lista_klas))
-    wprowadzana = input().lower()
-    if type(wprowadzana) != str:
-        raise ZlyTypDamychException(wprowadzana)
-    elif wprowadzana in lista_klas:
-        return wprowadzana
+        raise WrongChoiceException(entry)
+chosen_race = race_choice()
+print(chosen_race)
+def clas_choice():
+    print("Czas byś wybrał klasę dla swojej postaci. Tak jak poprzednio do wyboru masz 3 opcje {}".format(clas_list))
+    entry = input().lower()
+    if type(entry) != str:
+        raise WrongDataTypeException(entry)
+    elif entry in clas_list:
+        return entry
     else:
-        raise ZlyWyborException(wprowadzana)
-wybrana_klasa = wybor_klasy()
-print("Początkowe wartości cech postaci wynoszą: {}. Jednakże teraz nastąpi ich dostosowanie do podjętych przez ciebie wyborów.".format(cechy_postaci))
-def wybor_oreza():
-    if wybrana_klasa == "wojownik":
+        raise WrongChoiceException(entry)
+chosen_clas = clas_choice()
+print("Początkowe wartości cech postaci wynoszą: {}. Jednakże teraz nastąpi ich dostosowanie do podjętych przez ciebie wyborów.".format(char_traits))
+def weapon_choice():
+    if chosen_clas == "wojownik":
         print("Jako wojownik możesz dzierżyć miecz lub topór. Co wybierasz?")
-        wprowadzana = input()
-        if type(wprowadzana) != str:
-            raise ZlyTypDamychException(wprowadzana)
-        elif wprowadzana in dostepny_orez[wybrana_klasa]:
-            return wprowadzana
+        entry = input()
+        if type(entry) != str:
+            raise WrongDataTypeException(entry)
+        elif entry in weapons[chosen_clas]:
+            return entry
         else:
-            raise ZlyWyborException(wprowadzana)
-    elif wybrana_klasa == "strzelec":
+            raise WrongChoiceException(entry)
+    elif chosen_clas == "strzelec":
         print("Jako wojownik możesz dzierżyć łuk lub kuszę. Co wybierasz?")
-        wprowadzana = input()
-        if type(wprowadzana) != str:
-            raise ZlyTypDamychException(wprowadzana)
-        elif wprowadzana in dostepny_orez[wybrana_klasa]:
-            return wprowadzana
+        entry = input()
+        if type(entry) != str:
+            raise WrongDataTypeException(entry)
+        elif entry in weapons[chosen_clas]:
+            return entry
         else:
-            raise ZlyWyborException(wprowadzana)
+            raise WrongChoiceException(entry)
     else:
         print("Jako mag możesz dzierżyć wyłącznie kostur")
         return "kostur"
-wybrany_orez = wybor_oreza()
-class PrzypisanieCech():
-    """Klasa zmieniająca cechy tworzonej postaci ze względu na podjęte w kreatorze decyzje"""
-    def przypisanie_cech_rasowych(self):
-        if wybrana_rasa == "krasnolud":
-            cechy_postaci["Siła"] += 3
-            cechy_postaci["Zręczność"] -= 3
-            cechy_postaci["Intelekt"] -= 1
-        elif wybrana_rasa == "elf":
-            cechy_postaci["Zręczność"] += 1
-            cechy_postaci["Intelekt"] += 2
+chosen_weapon = weapon_choice()
+class Assignement_traits():
+
+    def assignement_traits_race(self):
+        if chosen_race == "krasnolud":
+            char_traits["Siła"] += 3
+            char_traits["Zręczność"] -= 3
+            char_traits["Intelekt"] -= 1
+        elif chosen_race == "elf":
+            char_traits["Zręczność"] += 1
+            char_traits["Intelekt"] += 2
         else:
-            cechy_postaci["Siła"] -= 1
-            cechy_postaci["Intelekt"] -= 1
-    def przypisanie_cech_klasowych(self):
-        if wybrana_klasa == "wojownik":
-            cechy_postaci["Siła"] += 2
-            cechy_postaci["Intelekt"] -= 3
-        elif wybrana_klasa == "strzelec":
-            cechy_postaci["Siła"] -= 1
-            cechy_postaci["Zręczność"] += 2
-            cechy_postaci["Intelekt"] -= 1
+            char_traits["Siła"] -= 1
+            char_traits["Intelekt"] -= 1
+    def assignement_traits_clas(self):
+        if chosen_clas == "wojownik":
+            char_traits["Siła"] += 2
+            char_traits["Intelekt"] -= 3
+        elif chosen_clas == "strzelec":
+            char_traits["Siła"] -= 1
+            char_traits["Zręczność"] += 2
+            char_traits["Intelekt"] -= 1
         else:
-            cechy_postaci["Siła"] -= 2
-            cechy_postaci["Zręczność"] -= 1
-            cechy_postaci["Intelekt"] += 3
-    def przypisanie_cechy_dominujacej(self):
-        if wybrana_klasa == "wojownik":
-            cecha_dominujaca = cechy_postaci["Siła"]
-            return cecha_dominujaca
-        elif wybrana_klasa == "strzelec":
-            cecha_dominujaca = cechy_postaci["Zręczność"]
-            return cecha_dominujaca
+            char_traits["Siła"] -= 2
+            char_traits["Zręczność"] -= 1
+            char_traits["Intelekt"] += 3
+    def assignement_traits_dominant(self):
+        if chosen_clas == "wojownik":
+            dominant = char_traits["Siła"]
+            return dominant
+        elif chosen_clas == "strzelec":
+            dominant = char_traits["Zręczność"]
+            return dominant
         else:
-            cecha_dominujaca = cechy_postaci["Intelekt"]
-            return cecha_dominujaca
-    def przypisanie_wartosci_oreza(self):
-        sila_ataku = random.randint(1, 10) + round((cecha_dominujaca / 2), 1)
-        return  sila_ataku
-    def ilosc_pkt_zycia(self):
-        b = cechy_postaci["Siła"] * 10 + 100
-        pkt_zycia = random.randint(100, b)
-        return  pkt_zycia
-    def ilosc_pkt_many(self):
-        b = cechy_postaci["Intelekt"] * 10 + 100
-        pkt_many = random.randint(100, b)
-        return  pkt_many
-    def ilosc_pkt_kondycji(self):
-        b = cechy_postaci["Zręczność"] * 10 + 100
-        pkt_kondycji = random.randint(100, b)
-        return  pkt_kondycji
-modyfikacja_cech = PrzypisanieCech()
-modyfikacja_cech.przypisanie_cech_klasowych()
-modyfikacja_cech.przypisanie_cech_rasowych()
-cecha_dominujaca = modyfikacja_cech.przypisanie_cechy_dominujacej()
-atak = modyfikacja_cech.przypisanie_wartosci_oreza()
-zycie = modyfikacja_cech.ilosc_pkt_zycia()
-mana = modyfikacja_cech.ilosc_pkt_many()
-kondycja = modyfikacja_cech.ilosc_pkt_kondycji()
-print("Obecne cechy Twojej postaci to: {}".format(cechy_postaci))
-print(modyfikacja_cech.przypisanie_wartosci_oreza())
-"""postac = {"imię":nazwa_postaci,
-          "rasa":wybrana_rasa,
-          "klasa":wybrana_klasa,
-          "statystki": {"ilość punktów życia": zycie,
+            dominant = char_traits["Intelekt"]
+            return dominant
+    def assignement_weapon_value(self):
+        stregth = random.randint(1, 10) + round((dominant / 2), 1)
+        return  stregth
+    def health_points(self):
+        b = char_traits["Siła"] * 10 + 100
+        health = random.randint(100, b)
+        return  health
+    def mana_points(self):
+        b = char_traits["Intelekt"] * 10 + 100
+        mana = random.randint(100, b)
+        return  mana
+    def condition_points(self):
+        b = char_traits["Zręczność"] * 10 + 100
+        condition = random.randint(100, b)
+        return  condition
+modification_traits = Assignement_traits()
+modification_traits.assignement_traits_clas()
+modification_traits.assignement_traits_race()
+dominant = modification_traits.assignement_traits_dominant()
+attack = modification_traits.assignement_weapon_value()
+health = modification_traits.health_points()
+mana = modification_traits.mana_points()
+condition = modification_traits.condition_points()
+print("Obecne cechy Twojej postaci to: {}".format(char_traits))
+print(modification_traits.assignement_weapon_value())
+"""postac = {"imię":ch_name,
+          "rasa":chosen_race,
+          "klasa":chosen_clas,
+          "statystyki": {"ilość punktów życia": health,
                         "ilość punktów many": mana,
-                        "ilość punktów ataku": atak,},
-          "broń":wybrany_orez,
-          "cechy": cechy_postaci}"""
-statystki = {"ilość punktów życia": zycie, "ilość punktów many": mana,"ilość punktów ataku": atak,}
-def historia():
+                        "ilość punktów attaku": attack,},
+          "broń":chosen_weapon,
+          "cechy": char_traits}"""
+stats = {"ilość punktów życia": health, "ilość punktów many": mana,"ilość punktów attaku": attack,}
+def story():
     print("Czy chcesz wprowadzić własną historię dla swojej postaci? T/N")
-    wprowadzana = input().upper()
-    if type(wprowadzana) != str:
-        raise ZlyTypDamychException(wprowadzana)
-    elif wprowadzana == "T":
+    entry = input().upper()
+    if type(entry) != str:
+        raise WrongDataTypeException(entry)
+    elif entry == "T":
         print("Wpisz swoją historię:")
         historia = input()
         return  historia
-    elif wprowadzana == "N":
+    elif entry == "N":
         historia = "Historia tej postaci nie została nigdy zapisana"
         return historia
     else:
-        raise ZlyWyborException(wprowadzana)
-zyciorys = historia()
-def zapis_postaci():
+        raise WrongChoiceException(entry)
+history = story()
+def save_character():
     print("Czy chcesz zapisać swoją postać? T/N")
-    wprowadzana = input().upper()
-    if type(wprowadzana) != str:
-        raise ZlyTypDamychException(wprowadzana)
-    elif wprowadzana == "T":
-        zapis_do_folderu()
-    elif wprowadzana == "N":
+    entry = input().upper()
+    if type(entry) != str:
+        raise WrongDataTypeException(entry)
+    elif entry == "T":
+        save_to_directory()
+    elif entry == "N":
         pass
     else:
-        raise ZlyWyborException(wprowadzana)
-def zapis_do_folderu():
-    path = "Utworzone postacie/{}/{}.txt ".format(nazwa_postaci,nazwa_postaci)
+        raise WrongChoiceException(entry)
+def save_to_directory():
+    path = "Utworzone postacie/{}/{}.txt ".format(ch_name,ch_name)
     dir_path = os.path.dirname(path)
     os.makedirs(dir_path)
     plik = open(path, "a+")
-    plik.write( "DANE POSTACI:\nImię: {}\nRasa: {}\nKlasa: {}\nCechy postaci:\n\tZręczność: {}\n\tSiła: {}\n\tIntelekt: {}\nStatystyki:\n\tŻycie: {}\n\tMana: {}\n\tAtak: {}\nHistoria:\n{}".format(nazwa_postaci, wybrana_rasa, wybrana_rasa, cechy_postaci["Zręczność"],cechy_postaci["Siła"], cechy_postaci["Intelekt"], statystki["ilość punktów życia"], statystki["ilość punktów many"], statystki["ilość punktów ataku"], zyciorys))
+    plik.write( "DANE POSTACI:\nImię: {}\nRasa: {}\nKlasa: {}\nCechy postaci:\n\tZręczność: {}\n\tSiła: {}\n\tIntelekt: {}\nStatystyki:\n\tŻycie: {}\n\tMana: {}\n\tattack: {}\nHistoria:\n{}".format(ch_name, chosen_race, chosen_race, char_traits["Zręczność"],char_traits["Siła"], char_traits["Intelekt"], stats["ilość punktów życia"], stats["ilość punktów many"], stats["ilość punktów attacku"], history))
     plik.close()
-zapis_postaci()
+save_character()
